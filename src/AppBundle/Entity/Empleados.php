a8b3969f50aedd30cc9b5baac5c2212ec33b16f1
Martinez201 <jesus.martinez.gonzalez1993@gmail.com>
2020-05-15 17:02:19 +0200

    añadida entidad empleados

---
+++ src/AppBundle/Entity/Empleados.php
@@ -0,0 +1,128 @@
+<?php
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="empleados")
+ */
+class Empleados
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     * @var int
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    private $nombre;
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    private $apellidos;
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    private $dni;
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    private $email;
+
+
+
+
+
+
+    /**
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @param string $nombre
+     * @return Empleados
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+
+    /**
+     * @param string $apellidos
+     * @return Empleados
+     */
+    public function setApellidos($apellidos)
+    {
+        $this->apellidos = $apellidos;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDni()
+    {
+        return $this->dni;
+    }
+
+    /**
+     * @param string $dni
+     * @return Empleados
+     */
+    public function setDni($dni)
+    {
+        $this->dni = $dni;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return Empleados
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+
+
+}
