3a12f136b0762d58f2fa1bf233c0caa881a1add7
Martinez201 <jesus.martinez.gonzalez1993@gmail.com>
2020-05-25 20:01:59 +0200

    corregido errores en los bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,15 +18,16 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Liuggio\ExcelBundle\LiuggioExcelBundle(),
+            new TFox\MpdfPortBundle\TFoxMpdfPortBundle(),
+            new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle()
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle();
             $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
-            $bundles[] = new TFox\MpdfPortBundle\TFoxMpdfPortBundle();
+
 
             if ('dev' === $this->getEnvironment()) {
                 $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
